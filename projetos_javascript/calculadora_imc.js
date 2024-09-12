97db285681aa0a6e877f650c341af65980904817
vnc <107007034+vinisrf@users.noreply.github.com>
2024-09-12 14:46:14 -0300

    calculadora_imc_(with function)
    calculadora_imc_(with function)

--- projetos_javascript/calculadora_imc.js
+++ projetos_javascript/calculadora_imc.js
@@ -1,21 +1,31 @@
 const input = require ('prompt-sync') ();
 
-var altura = parseFloat (input("DIGITE SUA ALTURA DE FORMA DECIMAL (USANDO '.'): "));
-var peso = parseFloat (input("DIGITE SEU PESO DE FORMA DECMAL (USANDO '.'): "));
-var imc =  peso / (altura*altura);
+let altura = parseFloat (input("DIGITE SUA ALTURA DE FORMA DECIMAL (USANDO '.'): "));
+let peso = parseFloat (input("DIGITE SEU PESO DE FORMA DECMAL (USANDO '.'): "));
+let imc = undefined;
 
-if(imc < 18.5){
-    console.log("seu IMC é de " + (imc.toFixed(2)) +" Declarado: ABAIXO DO PESO");
+function calc (){
+
+   imc = peso / (altura*altura);
+
+    if(imc < 18.5){
+        console.log("seu IMC é de " + (imc.toFixed(2)) +" Declarado: ABAIXO DO PESO");
+    }
+    else if(imc >= 18.5 && imc <= 24.9){
+        console.log("seu IMC é de " + (imc.toFixed(2))+ " Declarado: PESO NORMAL");
+    }
+    else if(imc >= 30 && imc <= 35){
+        console.log("seu IMC é de " +(imc.toFixed(2))+ " Declarado: OBESIDADE GRAU I");
+    }
+    else if(imc >= 30 && imc <= 40){
+        console.log("seu IMC é de " + (imc.toFixed(2))+ " Declarado: OBESIDADE GRAU II");
+    }
+    else if(imc > 40){
+        console.log("seu IMC é de " + (imc.toFixed(2))+ " Declarado: OBESIDADE GRAU III");
+    }
+
+    
 }
-else if(imc >= 18.5 && imc <= 24.9){
-    console.log("seu IMC é de " + (imc.toFixed(2))+ " Declarado: PESO NORMAL");
-}
-else if(imc >= 30 && imc <= 35){
-    console.log("seu IMC é de " +(imc.toFixed(2))+ " Declarado: OBESIDADE GRAU I");
-}
-else if(imc >= 30 && imc <= 40){
-    console.log("seu IMC é de " + (imc.toFixed(2))+ " Declarado: OBESIDADE GRAU II");
-}
-else if(imc > 40){
-    console.log("seu IMC é de " + (imc.toFixed(2))+ " Declarado: OBESIDADE GRAU III");
-}
+
+calc();
+
